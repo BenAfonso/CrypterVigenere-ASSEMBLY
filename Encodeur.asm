559243f9a1079d75c4c1ca2450199c4a9c60d665
Darkyler <Darkyler57@gmail.com>
2015-11-29 20:32:36 +0100

    Final commit

--- Encodeur.asm
+++ Encodeur.asm
@@ -10,19 +10,20 @@
     .data
     inputKey: .asciiz "Entrez une cle d'encodage (50 caracteres maximum): "
     chosenKey: .asciiz "La cle choisie est : "
-    firstMenu: .asciiz "\nVoulez vous encoder (Entrez 1) ou decoder (Entrez 2): "
-    cryptChoice: .asciiz "Voulez vous encoder un fichier texte (Entrez 1) ou une phrase (Entrez 2): "
+    firstMenu: .asciiz "============== MENU ==============\n1. Encoder\n2. Decoder\nVotre choix: "
+    cryptChoice: .asciiz "\n1. Fichier texte\n2. Phrase\nVotre choix: "
     inputText: .asciiz "\nVeuillez saisir le texte: "
     inputFile: .asciiz "\nVeuillez saisir le fichier texte: "
-    resText: .asciiz "\nLe resultat est: "
-    resFile: .asciiz "\nLe resultat est stocké dans: "
+    resText: .asciiz "\nLe resultat est:\n"
+    resFile: .asciiz "\n Ne vous inquietez pas le resultat est stocké dans output.txt"
+    resEnd: .asciiz "\n============== MENU ==============\n1. Quitter\n2.Recommencer\nVotre choix: "
     file: .space 50
     output: .asciiz "output.txt"
-    text: .space 1000
+    text: .space 10000
     key: .space 50
     keyArray: .space 50
-    textArray: .space 1000
-    encode: .space 100
+    textArray: .space 10000
+    encode: .space 10000
      
     .text
      
@@ -41,11 +42,10 @@
             beq $t1,2,cryptText
             beq $s1,1,encoder
             beq $s1,2,decoder
+            jal stockerResultat
             
 		
-            # Fin du programme
-            j end
-            li $v0,10
+
            
     #########################################################################
     #################### INTERACTIONS UTILISATEUR ###########################
@@ -71,11 +71,11 @@
 	sw $ra , ( $sp )
             la $a0,inputKey
             li $v0,4
-            syscall
+            syscall	# Affichage du message inputKey
             la $a0,key
             la $a1,50
             li $v0,8
-            syscall
+            syscall	# Demande la clé à l'utilisateur
             la $a1, keyArray
             jal stockInArray
         lw $ra , ( $sp )	# On restaure l'adresse de la routine principale
@@ -90,10 +90,10 @@
     affichage:
             la $a0,chosenKey
             li $v0, 4
-            syscall
+            syscall	# On affiche un message 
             la $a0,key
             li $v0,4
-            syscall
+            syscall	# On affiche la clé choisie par l'utilisateur
             jr $ra
      
     #########################################################
@@ -155,29 +155,29 @@
 		
 
 				
-		loopC:				#remplace le "\n" par le caractï¿½re nul pour permettre la reconnaissance de fin de chaï¿½ne
+		loop:				#remplace le "\n" par le caractï¿½re nul pour permettre la reconnaissance de fin de chaï¿½ne
 			lb $t0, ($a0)
-			beq $t0, 10, changeC
+			beq $t0, 10, change
 			addiu $a0, $a0, 1
-			j loopC
+			j loop
 		
-		changeC:
+		change:
 			li $t0, 0
 			sb $t0, ($a0)
 			
-			li $v0, 13			#ouvre le fichier demandï¿½
+			li $v0, 13			
 			la $a0, file
 			li $a1, 0
 			li $a2,0
-			syscall		
+			syscall			# On ouvre le fichier en mode lecture
 			move $s0, $v0
 			
- 		readC:					#lire les 1000 premiers caractï¿½res du fichier
+ 						
 			li $v0, 14
 			move $a0, $s0
 			la $a1, text
 			li $a2, 1000
-			syscall		# Lecture du buffer texte
+			syscall		# Lecture des 10000 premiers caractères
 			move $a0,$a1 # a0 Contient Text
 			la $a1, textArray	# a1 Contient Array
 			jal stockInArray			#appelle la routine pour stocker le contenu du fichier
@@ -186,29 +186,12 @@
 			li $v0, 16	# Fermeture du fichier
 			move $a0, $s0
 			syscall
+			
 			lw $ra,($sp)
 			addi $sp, $sp, 4		
 			jr $ra
 
-    #########################################################
-    ############## STOCKER LE RESULTAT A FAIRE ##############
-    #########################################################	    	
-    	
-    stockerResultat:
-    	li $v0, 13			#ouvre le fichier demandï¿½
-	la $a0, output
-	li $a1, 0
-	li $a2,0
-	syscall		
-	move $s0, $v0
-	
-	# Ecriture dans le fichier OUTPUT
-	
-	li $v0,15
-	move $a0,$s0
-	la $a1,text
-	la $a2,1000
-	syscall
+
  		
 		
     #########################################################
@@ -239,6 +222,7 @@
     # Cle stockee dans un tableau keyArray
     # Texte stocke dans un tableau textArray
  	encoder:
+
  		li $v0,4
  		la $a0,resText
  		syscall
@@ -257,7 +241,7 @@
 		beq $t3,13,nochange	# Fin de ligne :: Pas d'opérations
 		beq $t3,10,nochange	# Fin de ligne :: Pas d'opérations
 		addiu $t1,$t1,1 # On incremente les deux
-		beq $t3,0,end # On arrete quand le caractère courant est 0
+		beq $t3,0,endMenu # On arrete quand le caractère courant est 0
 		# Modulo + Offset avec des additions / soustractions
 		add $t4,$t4,$t3
 		bge $t4,220,plus2
@@ -276,15 +260,18 @@
 		la $a0,($t3) # On affiche la valeur de t3 (cle)
 		li $v0,11
 		syscall		# On affiche la lettre encodee
+		sb $a0,($t2)
+		addiu $t2,$t2,1
 		j for3
 	next:
 		la $a0,($t4) # On affiche la valeur de t3 (cle)
 		li $v0,11
 		syscall		# On affiche la lettre encodee
-		sb $a0,encode
+		sb $a0,($t2)
+		addiu $t2,$t2,1
 		
 		bne $t3,0,for3 #On boucle tant que t2 n'est pas fini (texte)
-		jr $ra
+
 		
 		
 #########################################################
@@ -294,6 +281,8 @@
     # Clï¿½ stockï¿½e dans un tableau keyArray
     # Texte stockï¿½ dans un tableau textArray
  	decoder:
+
+		
  		li $v0,4
  		la $a0,resText
  		syscall
@@ -310,14 +299,14 @@
 		beq $t3,13,nochange2	# Fin de ligne :: Pas d'opérations
 		beq $t3,10,nochange2	# Fin de ligne :: Pas d'opérations
 		addiu $t1,$t1,1 # On incrï¿½mente la clé
-		beq $t3,0,end	
+		beq $t3,0,endMenu	
 		
 		# Modulo + Offset avec des additions / soustractions
 		neg $t4,$t4
 		add $t4,$t3,$t4
 		ble $t4,-63,plus3 
 		ble $t4,31,plus4
-		jal next2
+		j next2
 	plus3:
 		addiu $t4,$t4,188 # Si $t4 est < -64, on fait +188
 		j next2
@@ -328,25 +317,68 @@
 		la $a0,($t3) # On affiche la valeur de t3 (cle)
 		li $v0,11
 		syscall		# On affiche la lettre encodee
+		sb $a0,($t2)
+		addiu $t2,$t2,1
 		j for4
 	next2:
 		la $a0,($t4) # On affiche la valeur de t4
 		li $v0,11
 		syscall		# On affiche la lettre decodee
-		
-
+		sb $a0,($t2)
+		addiu $t2,$t2,1
 		bne $t3,0,for4	 #On boucle tant que t2 n'est pas fini (texte)
-		j end
+
 		
 	
-		
+    #########################################################
+    ############## STOCKER LE RESULTAT A FAIRE ##############
+    #########################################################	    	
+    	
+    stockerResultat:
+	li $v0, 13			#ouvre le fichier demandï¿½
+	la $a0, output
+	li $a1, 1
+	li $a2,0
+	syscall		
+	move $s0, $v0
+	
+	# Ecriture dans le fichier OUTPUT
+	
+	li $v0,15
+	move $a0,$s0
+	la $a1,encode
+	la $a2,1000
+	syscall	
+	
+	li $v0, 16	# Fermeture du fichier
+	move $a0, $s0
+	syscall
+			
+	la $a0,resFile
+	li $v0,4
+	syscall
+
+	jr $ra
  		
    	
    	
    	
     # Fin du programme     
+    endMenu:
+    	jal stockerResultat
+    	la $a0,resEnd
+    	li $v0,4
+    	syscall		# Affichage du menu de fin
+    	li $v0,5
+    	syscall		# Prompt du choix utilisateur
+    	beq $v0,1,end
+    	beq $v0,2,main
+    	
+    	
     end:
-    	j main
+    	li $v0,10
+    	syscall		# Fermeture du programme
+ 
 
     
     	
